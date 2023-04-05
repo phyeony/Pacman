638b59d59c40075604c9c9b38ad11ddce4fb9d28
phyeony <hyeonyoung_park@sfu.ca>
2023-04-05 10:34:11 -0700

    fix

--- ledDisplay.h
+++ ledDisplay.h
@@ -1,7 +1,7 @@
 #ifndef _LEDDISPLAY_H_
 #define _LEDDISPLAY_H_
 #include "map.h"
-typedef void (*LEDDisplayCallback)(Tile temp[][COLUMN_SIZE], int *currentScore, int *highScore);
+typedef void (*LEDDisplayCallback)(Tile temp[][COLUMN_SIZE]);
 void LedDisplay_init(void);
 void LedDisplay_cleanup(void);
 void LedDisplay_quitGame();
